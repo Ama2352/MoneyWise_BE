b52e741acbf8856310672a6f75955861afb5c3d2
Hồ Nguyễn Minh Sang <165858646+Ama2352@users.noreply.github.com>
2025-05-09 14:19:06 +0700

    first init

---
+++ src/main/java/JavaProject/MoneyManagement_BE_SE330/MoneyManagementBeSe330Application.java
@@ -0,0 +1,13 @@
+package JavaProject.MoneyManagement_BE_SE330;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyManagementBeSe330Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyManagementBeSe330Application.class, args);
+	}
+
+}
---
+++ src/test/java/JavaProject/MoneyManagement_BE_SE330/MoneyManagementBeSe330ApplicationTests.java
@@ -0,0 +1,13 @@
+package JavaProject.MoneyManagement_BE_SE330;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyManagementBeSe330ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
